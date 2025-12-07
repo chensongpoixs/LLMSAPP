9c30bb9e77883ec7d2ae5d221d3437e489d42454
chensong <15850774503@163.com>
2025-12-07 19:10:04 +0800

    [分词的Tokenizer的 编码和解码 函数实现]

--- 文本与分词/Tokenizer.py
+++ 文本与分词/Tokenizer.py
@@ -3,6 +3,7 @@
 @date: 2025年12月7日
 Docstring for 文本与分词.Tokenizer
 '''
+import re;
 
 class SimpleTokenizer:
     """A simple tokenizer class for demonstration purposes.
@@ -13,27 +14,43 @@ class SimpleTokenizer:
 
     def __init__(self, filename):
         self.filename = filename
-        self.token_w2t = None; # word to token mapping
-        self.token_t2w = None; # token to word mapping
+        self.vocab_w2t = None; # word to token mapping
+        self.vocab_t2w = None; # token to word mapping
 
-        lines = "";
+        row_data = "";
         with open(self.filename, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
+            row_data = f.read()
            
         # 去重、排序
-        vocab = sorted(set([word for line in lines for word in line.strip().split()]))  ; # 简单按空格分词
-        self.token_w2t = {word: idx for idx, word in enumerate(vocab)};
-        # token_t2w
-        self.token_t2w = {idx: word for idx, word in self.token_w2t.items()};
+       # all_word = sorted(set([token for line in row_data for token in line.strip().split()]))  ; # 简单按空格分词
+        all_word = sorted(set(list(row_data)));
+        # 打印前100个词
+        # print("First 100 tokens in vocabulary:");
+        # for i, token in enumerate(all_word[:100]):
+        #     print(f"{i}: {token}");
+        all_word.extend(["<|unk|>", "<|endoftext|>"]);
+        #all_word.extend(['<PAD>', '<UNK>']); # 添加特殊token
+        # token_w2t
+        self.vocab_w2t = {token: idx for idx, token in enumerate(all_word)};
+        self.vocab_t2w = {idx: token for (token, idx) in self.vocab_w2t.items()};
 
     def encode(self, text):
         """Encode text into a list of token IDs."""
-        words = text.strip().split()
-        return [self.token_w2t.get(word, -1) for word in words]; # 未知词返回-1
+        ids = [];
+        # 以单字符为单位进行分词 
+        
+        words = re.findall(rf'{re.escape("<|endoftext|>")}|.', text);
+        # 
+        words = [w.strip() for w in words if w.strip()];
+        words = [ w if w in self.vocab_w2t else "<|unk|>" for w in words];
+        ids = [self.vocab_w2t[w] for w in words];
+        return ids
 
     def decode(self, token_ids):
-        """Decode a list of token IDs back into text."""
-        words = [self.token_t2w.get(token_id, '<UNK>') for token_id in token_ids]; # 未知token返回<UNK>
-        return ' '.join(words);
+        text = ""
+        # for idx in ids:
+        #     print(self.vocab_t2w[idx])
+        text = text.join([self.vocab_t2w[idx] for idx in token_ids]);
+        return text
          
  
